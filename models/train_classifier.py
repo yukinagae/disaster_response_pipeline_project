835c1ff090460327816773569216a731450cf84d
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-04-12 15:19:25 +0900

    Implemented ML train module

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,81 @@
 import sys
+import re
+import pickle
+import pandas as pd
+from sqlalchemy import create_engine
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, accuracy_score
 
 
 def load_data(database_filepath):
-    pass
+
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('disaster_response', con=engine)
+
+    # split data into X, Y, category_names
+    X = df['message']
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = Y.columns.values
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+
+    lower_text = text.lower()                                                                         # lower text
+    cleaned_text = re.sub(r"[^a-z0-9]", " ", lower_text)                                              # clean text
+    tokens = word_tokenize(cleaned_text)                                                              # tokenize text
+    tokens_withou_stop_words = [token for token in tokens if token not in stopwords.words("english")] # remove stop words
+    tokens_lemmatized = [WordNetLemmatizer().lemmatize(token) for token in tokens_withou_stop_words]  # lemmatize tokens
+    
+    return tokens_lemmatized
 
 
 def build_model():
-    pass
+
+    pipeline =  Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    # grid search parameters
+    parameters = {
+        'clf__estimator__n_estimators': [50, 100],
+        'clf__estimator__max_depth': [3, 5, 8]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+
+    Y_pred = model.predict(X_test)
+    
+    for i, column in enumerate(Y_test.columns):
+        print("Category: {}".format(category_names[i]))
+        print("Accuracy Score: {:.2f}".format(accuracy_score(Y_test.values[i], Y_pred[i])))
+        print("Classification Report: \n {}".format(classification_report(Y_test.values[i], Y_pred[i])))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, "wb"))
 
 
 def main():
